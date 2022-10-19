24519eb2d15f7ccab1e575256a3702f17c586f92
Ivaylo Ushev <iushev@yahoo.com>
2022-10-19 12:44:36 +0300

    Fix tests

--- src/__tests__/models/Assignment.test.ts
+++ src/__tests__/models/Assignment.test.ts
@@ -1,4 +1,4 @@
-import faker from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
 import { Sequelize } from "sequelize";
 
 import { ItemType, Identity } from "@iushev/rbac";
--- src/__tests__/models/Role.test.ts
+++ src/__tests__/models/Role.test.ts
@@ -1,4 +1,4 @@
-import faker from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
 import { Sequelize } from "sequelize";
 
 import initModels, { RoleModel } from "../../models";
